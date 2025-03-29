4d65b738eac4359ca34cf12e8a1d8574359e34d3
AlexPT2k22 <xande6871@gmail.com>
2025-03-29 19:20:15 +0000

    Update axios to version 1.8.4 and add API fetch in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
 import Login from "./components/Login.jsx";
 import Slides from "./components/Slides.jsx";
+import axios from "axios";
+import { use, useEffect } from "react";
 
 function App() {
+  const fetchAPI_TESTE = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    console.log(response.data.fruits);
+  };
+
+  useEffect(() => {
+    fetchAPI_TESTE();
+  }, []);
+
   return (
     <div className="login-container">
       {/*Parte esquerda com os slides*/}
