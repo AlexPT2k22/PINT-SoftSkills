aa12b0ac3d03f533496fbb962368003e78c5de60
AlexPT2k22 <xande6871@gmail.com>
2025-03-30 17:38:55 +0100

    Criado JSON de teste para simular db

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
-//teste
-const users = [
-  {
-    Name: "Alex",
-    Pass: "12eq",
-  },
-];
+const fs = require("fs");
+const path = require("path");
+const usersFilePath = path.join(__dirname, "../database/users.json");
+let users = require(usersFilePath);
 
 // rota para /users
 router.get("/", (req, res) => {
@@ -16,8 +12,23 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const user = req.body; //teste
-  users.push(user); //teste
-  res.send(`User entrou na BD: ${user.Name} com a pass ${user.Pass}`); //teste
+  if (!user.Name || !user.Pass) {
+    return res
+      .status(400)
+      .json({ error: "O campo Name e Pass são obrigatórios!" });
+  }
+  users.push(user); // Adiciona o novo utilizador ao array de utilizadores
+  fs.writeFile(
+    usersFilePath,
+    JSON.stringify(users, null, 2),
+    "utf-8",
+    (err) => {
+      if (err) {
+        return res.status(500).json({ error: "Erro ao guardar utilizador!" });
+      }
+      res.json({ message: `Utilizador ${user.Name} adicionado com sucesso!` });
+    }
+  );
 });
 
 module.exports = router;
