5b412bc1432a824dd597425ea0c699cbace94b7e
AlexPT2k22 <xande6871@gmail.com>
2025-04-02 22:41:51 +0100

    Refactor LinkedIn callback to improve user existence check and streamline redirection logic

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,21 +2,19 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 require("dotenv").config();
-const fs = require("fs");
 const path = require("path");
 const usersFilePath = path.join(__dirname, "../database/users.json");
 let users = require(usersFilePath);
 const linkedin_url = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URL}&state=foobar&scope=openid%20profile%20email`;
 
 // "/auth"
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
   res.json("Auth");
 });
 
-router.get("/linkedin", (req, res) => {
+router.get("/linkedin", (_, res) => {
   res.redirect(linkedin_url);
 });
-
 router.get("/linkedin/callback", async (req, res) => {
   const { code } = req.query;
   //console.log("Code:", code);
@@ -46,17 +44,34 @@ router.get("/linkedin/callback", async (req, res) => {
     const accessToken = tokenResponse.data.access_token;
     //console.log("Access Token:", accessToken);
 
-    const profileResponse = await axios.get("https://api.linkedin.com/v2/userinfo", {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
+    const profileResponse = await axios.get(
+      "https://api.linkedin.com/v2/userinfo",
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
 
     const userProfile = profileResponse.data;
-    const username = userProfile.name;
+    const LinkedinUsername = userProfile.name;
     const email = userProfile.email;
     //console.log("Username:", username);
     //console.log("Email:", email);
 
-    res.redirect(`http://localhost:8080/dashboard?username=${username}`);
+    //res.redirect(`http://localhost:8080/dashboard?username=${username}`);
+
+    // verificar se o user existe
+    const userExist = users.find(
+      (u) => u.Email === email && u.Username === LinkedinUsername
+    );
+    if (userExist) {
+      console.log(`Sucesso`);
+      return res.redirect(
+        `http://localhost:8080/dashboard?username=${LinkedinUsername}`
+      );
+    } else {
+      //return res.status(401).json({ error: "User não existe" });
+      // TODO: adicionar o user a db 
+    }
   } catch (error) {
     console.error("Error:", error.response?.data || error.message);
     res.status(500).json({ error: "Erro ao autenticar com o LinkedIn!" });
