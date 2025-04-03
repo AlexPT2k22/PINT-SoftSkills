05440f3ddb1b7ec3c17df3a469cfc52ddd5564da
AlexPT2k22 <xande6871@gmail.com>
2025-04-04 00:00:07 +0100

    Implement user registration and login routes; remove Supabase integration from user routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -71,11 +71,78 @@ router.get("/linkedin/callback", async (req, res) => {
       );
     } else {
       return res.status(401).json({ error: "User não existe" });
-      // TODO: adicionar o user a db 
+      // TODO: adicionar o user a db
     }
   } catch (error) {
     console.error("Error:", error.response?.data || error.message);
     res.status(500).json({ error: "Erro ao autenticar com o LinkedIn!" });
+  }
+});
+
+router.post("/register", (req, res) => {
+  const user = {
+    Username: req.body.Username,
+    Password: req.body.Password,
+    Email: req.body.Email,
+    LinkedIN: "", // para guardar o link do linkedin
+    Type: "user",
+  }; //teste
+  if (!user.Username || !user.Password || !user.Email) {
+    return res
+      .status(400)
+      .json({ error: "O campo Username, email e password são obrigatórios!" });
+  }
+
+  // Verifica se o utilizador já existe
+  const existingUser = users.find((u) => u.Username === user.Username);
+  if (existingUser) {
+    console.log(`Utilizador ${user.Username} já existe!`);
+    return res.status(400).json({ error: "Utilizador já existe!" });
+  }
+  // Verifica se o email já existe
+  const existingEmail = users.find((u) => u.Email === user.Email);
+  if (existingEmail) {
+    console.log(`Email ${user.Email} já existe!`);
+    return res.status(400).json({ error: "Email já existe!" });
+  }
+
+  users.push(user); // Adiciona o novo utilizador ao array de utilizadores
+  fs.writeFile(
+    usersFilePath,
+    JSON.stringify(users, null, 2),
+    "utf-8",
+    (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Erro ao guardar utilizador!" });
+      }
+      res.json({
+        message: `Utilizador ${user.Username} adicionado com sucesso!`,
+      });
+      console.log(`Utilizador ${user.Username} adicionado com sucesso!`);
+    }
+  );
+});
+
+router.post("/login", (req, res) => {
+  const user = req.body;
+  if (!user.Email || !user.Password) {
+    // verificar os campos obrigatorios
+    return res
+      .status(500)
+      .json({ error: "O campo Email e password são obrigatórios!" });
+  }
+
+  // verificar se o user existe
+  const userExist = users.find(
+    (u) => u.Email === user.Email && u.Password === user.Password
+  );
+  if (userExist) {
+    console.log(`Sucesso`);
+    res.sendStatus(200);
+    // redirecionar para a dashboard
+  } else {
+    res.sendStatus(401).json({ error: "Email ou password errados" });
   }
 });
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,114 +1,13 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
-const fs = require("fs");
 const path = require("path");
 const usersFilePath = path.join(__dirname, "../database/users.json");
 let users = require(usersFilePath);
 
 // rota para /user
 router.get("/", async (req, res) => {
-  //res.send(users); //teste
-  const { data, error } = await supabase.from("users").select();
-
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
-  res.json(data);
-});
-
-// rota para /user/signup
-router.post("/signup", async (req, res) => {
-  const { username, email, password } = req.body;
-  console.log("Username:", username);
-  console.log("Email:", email);
-  if (!username || !email || !password) {
-    return res
-      .status(400)
-      .json({ error: "Username, email e password são obrigatórios!" });
-  }
-
-  const { data, error } = await supabase.auth.signUp({
-    email: email,
-    password: password,
-    options: {
-      data: {
-        username: username,
-      },
-    },
-  });
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
-  res.json(data);
-});
-
-// "/user/register"
-router.post("/register", (req, res) => {
-  const user = {
-    Username: req.body.Username,
-    Password: req.body.Password,
-    Email: req.body.Email,
-    LinkedIN: "", // para guardar o link do linkedin
-    Type: "user",
-  }; //teste
-  if (!user.Username || !user.Password || !user.Email) {
-    return res
-      .status(400)
-      .json({ error: "O campo Username, email e password são obrigatórios!" });
-  }
-
-  // Verifica se o utilizador já existe
-  const existingUser = users.find((u) => u.Username === user.Username);
-  if (existingUser) {
-    console.log(`Utilizador ${user.Username} já existe!`);
-    return res.status(400).json({ error: "Utilizador já existe!" });
-  }
-  // Verifica se o email já existe
-  const existingEmail = users.find((u) => u.Email === user.Email);
-  if (existingEmail) {
-    console.log(`Email ${user.Email} já existe!`);
-    return res.status(400).json({ error: "Email já existe!" });
-  }
-
-  users.push(user); // Adiciona o novo utilizador ao array de utilizadores
-  fs.writeFile(
-    usersFilePath,
-    JSON.stringify(users, null, 2),
-    "utf-8",
-    (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ error: "Erro ao guardar utilizador!" });
-      }
-      res.json({
-        message: `Utilizador ${user.Username} adicionado com sucesso!`,
-      });
-      console.log(`Utilizador ${user.Username} adicionado com sucesso!`);
-    }
-  );
-});
-
-router.post("/login", (req, res) => {
-  const user = req.body;
-  if (!user.Email || !user.Password) {
-    // verificar os campos obrigatorios
-    return res
-      .status(500)
-      .json({ error: "O campo Email e password são obrigatórios!" });
-  }
-
-  // verificar se o user existe
-  const userExist = users.find(
-    (u) => u.Email === user.Email && u.Password === user.Password
-  );
-  if (userExist) {
-    console.log(`Sucesso`);
-    res.sendStatus(200);
-    // redirecionar para a dashboard
-  } else {
-    res.sendStatus(401).json({ error: "Email ou password errados" });
-  }
+  res.send(users); //teste
 });
 
 module.exports = router;
