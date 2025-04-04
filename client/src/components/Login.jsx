239efe2dfe9e74fd9d6ae866108342cfaa147c6d
AlexPT2k22 <xande6871@gmail.com>
2025-04-04 14:54:00 +0100

    Fix API URL in Login component for production environment

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
     e.preventDefault(); // Evita que a página recarregue
     setError(null); // Limpa o erro antes de fazer a requisição
 
-    let url = process.env.NODE_ENV === "production" ? "https://pint-softskills-api.onrender.com" : "http://localhost:8080/";
+    let url = process.env.NODE_ENV === "production" ? "https://pint-softskills-api.onrender.com/" : "http://localhost:8080/";
     let method = "POST";
     let body = {};
 
