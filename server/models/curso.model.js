8d6b5f49680fc984bddff8a26d5bc2e630e834c3
AlexPT2k22 <xande6871@gmail.com>
2025-04-17 17:04:24 +0100

    Add Curso model with fields for ID, area, name, objectives, difficulty, and creation date

---
+++ server/models/curso.model.js
@@ -0,0 +1,45 @@
+const { Sequelize } = require("sequelize");
+const { sequelize } = require("../database/database.js");
+
+module.exports = (sequelize) => {
+  const Curso = sequelize.define(
+    "Curso",
+    {
+      ID_CURSO: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      ID_AREA: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "AREA",
+          key: "ID_AREA",
+        },
+      },
+      NOME: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      DESCRICAO_OBJETIVOS__: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      DIFICULDADE_CURSO__: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      DATA_CRIACAO__: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "CURSO",
+      timestamps: false,
+    }
+  );
+};
+
+module.exports = Curso;
