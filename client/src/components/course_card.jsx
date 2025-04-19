970cdc6e73212cf0f2302912054a5834f1602b34
AlexPT2k22 <xande6871@gmail.com>
2025-04-20 00:40:59 +0100

    Refactor CourseCard component to improve variable handling and layout; update logic for displaying course availability and certificate status.

--- client/src/components/course_card.jsx
+++ client/src/components/course_card.jsx
@@ -3,27 +3,29 @@ import { Award } from "lucide-react";
 import "../styles/course_card.css";
 
 function CourseCard({ course }) {
-  const {
-    NOME,
-    ID_CURSO,
-    CURSO_ASSINCRONO,
-    CURSO_SINCRONO,
-    tempoTotal,
-    DATA_INICIO,
-    DATA_FIM,
-    LIMITE_VAGAS_INT__,
-  } = course;
+  const { NOME, CURSO_ASSINCRONO, CURSO_SINCRONO } = course;
 
   const formatDate = (dateString) => {
     const options = { month: "numeric", day: "numeric" };
     return new Date(dateString).toLocaleDateString("pt-PT", options);
   };
 
-  var tipo = CURSO_ASSINCRONO ? "Assíncrono" : "Sincrono";
+  const LIMITE_VAGAS =
+    CURSO_SINCRONO?.INSCRICAO_SINCRONO?.LIMITE_VAGAS_INT__ || 0;
+
+  const DATA_INICIO = CURSO_SINCRONO?.DATA_INICIO || "NA";
+  const DATA_FIM = CURSO_SINCRONO?.DATA_FIM || "NA";
+  const tempoTotal = CURSO_SINCRONO?.TEMPO_TOTAL || "N/A";
+
+  const tipo = CURSO_ASSINCRONO
+    ? "Assíncrono"
+    : CURSO_SINCRONO
+    ? "Sincrono"
+    : "Não especificado";
 
   return (
     <div className="card h-100 course-card">
-      {LIMITE_VAGAS_INT__ < 5 && (
+      {CURSO_SINCRONO && LIMITE_VAGAS < 5 && (
         <div className="z-1 position-absolute p-2">
           <span className="badge text-bg-info position-absolute p-2 fs-6">
             Mais Popular!
@@ -51,16 +53,14 @@ function CourseCard({ course }) {
         </div>
         <div className="d-flex justify-content-between align-items-center">
           <p className="card-text mb-0">
-            {tipo === "Sincrono"
-              ? "Vagas restantes: " + LIMITE_VAGAS_INT__
-              : ""}
+            {tipo === "Sincrono" ? "Vagas restantes: " + LIMITE_VAGAS : ""}
           </p>
         </div>
-        <div className="d-flex justify-content-between align-items-center mt-4">
-          <div className="card-text mb-0 d-flex align-items-center">
-            <Award size={21} color="#39639C" />
-            <p className="card-certificado ms-1 mb-0">Certificado disponivel</p>
-          </div>
+      </div>
+      <div className="card-footer bg-white">
+        <div className="card-text mb-0 d-flex align-items-center">
+          <Award size={21} color="#39639C" />
+          <p className="card-certificado ms-1 mb-0">Certificado disponivel</p>
         </div>
       </div>
     </div>
